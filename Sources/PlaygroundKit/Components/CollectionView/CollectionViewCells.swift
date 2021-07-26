540f51e83257f4df8478d83fd745301543102332
mgacy <matt@mgacy.com>
2021-07-25 20:58:25 -0700

    Add BaseImageCell

--- Sources/PlaygroundKit/Components/CollectionView/CollectionViewCells.swift
+++ Sources/PlaygroundKit/Components/CollectionView/CollectionViewCells.swift
@@ -10,6 +10,7 @@ import UIKit
 
 // MARK: - SimpleCollectionViewCell
 public class SimpleCollectionViewCell: UICollectionViewCell {
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .cyan
@@ -26,5 +27,44 @@ public extension SimpleCollectionViewCell: CellConfigurable {
    public func configure(with item: MyItem) {
        backgroundColor = item.color
    }
+}
+*/
+
+open class BaseImageCell: UICollectionViewCell {
+
+    // MARK: - Subviews
+
+    public let imageView: UIImageView = {
+        let view = UIImageView()
+        view.contentMode = .scaleAspectFit
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        return view
+    }()
+
+    // MARK: - Lifecycle
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        imageView.frame = frame
+        contentView.addSubview(imageView)
+        setup()
+    }
+
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    open func setup() {
+        // ...
+    }
+}
+/*
+// MARK: - CellConfigurable
+extension BaseImageCell: CellConfigurable {
+    public typealias ModelType = MyItem
+
+    public func configure(with item: MyItem) {
+        backgroundColor = item.color
+    }
 }
 */
